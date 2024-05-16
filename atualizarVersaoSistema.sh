b9b599ef3737852908c721d1a87a9d1e643f413a
kivyadegaspare <kivya.degaspare@a7.net.br>
2024-05-16 12:37:04 -0300

    Update atualizarVersaoSistema.sh

--- atualizarVersaoSistema.sh
+++ atualizarVersaoSistema.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Criado por Kivya Pottechi
 WILDFLY_STANDALONE=/usr/wildfly/standalone
 CHINCHILA_CLIENT_DIR=$WILDFLY_STANDALONE/chinchila-client
 CHINCHILA_PDV_DIR=$WILDFLY_STANDALONE/chinchila-pdv
