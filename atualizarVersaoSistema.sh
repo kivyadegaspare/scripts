1f29610ac36cde591ced3cf76c76603c8820d8ab
kivyadegaspare <kivya.degaspare@a7.net.br>
2024-11-15 08:06:14 -0300

    Update atualizarVersaoSistema.sh

--- atualizarVersaoSistema.sh
+++ atualizarVersaoSistema.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#Criado por Kivya Pottechi
+# Criado por Kivya Pottechi
+
 WILDFLY_STANDALONE=/usr/wildfly/standalone
 CHINCHILA_CLIENT_DIR=$WILDFLY_STANDALONE/chinchila-client
 CHINCHILA_PDV_DIR=$WILDFLY_STANDALONE/chinchila-pdv
@@ -7,17 +8,15 @@ CHINCHILA_BROKER_DIR=$WILDFLY_STANDALONE/chinchila-broker
 CHINCHILA_UPDATE_PGKS_DIR=$WILDFLY_STANDALONE/chinchila-update-pkgs
 DEPLOYMENTS_DIR=$WILDFLY_STANDALONE/deployments
 
-# Function to display message in cyan color
+# Funções de mensagem
 function msg(){
   echo -e "\e[36;1m$1\e[0m"
 }
 
-# Function to display error message in red color
 function error(){
   echo -e "\e[31;1m$1\e[0m"
 }
 
-# Function to display warning message in yellow color
 function warn(){
   echo -e "\e[33;1m$1\e[0m"
 }
@@ -27,11 +26,11 @@ if [ $UID -eq 0 ]; then
   exit 1
 fi
 
-# Asking for the link to the package
+# Solicitar o link do pacote
 msg "Insira o link do pacote de atualização:"
 read -p "Link: " package_link
 
-# Downloading the package
+# Baixar o pacote
 msg "Baixando pacote de atualização..."
 wget -c "$package_link" -P "$CHINCHILA_UPDATE_PGKS_DIR" || {
   error "Falha ao baixar o pacote. Verifique o link e se há conectividade com a internet."
@@ -40,22 +39,44 @@ wget -c "$package_link" -P "$CHINCHILA_UPDATE_PGKS_DIR" || {
 
 PACOTE="$CHINCHILA_UPDATE_PGKS_DIR/$(basename "$package_link")"
 
-# Update the system
-msg "Atualizando o sistema com o pacote baixado..."
-sudo service wildfly stop || {
-  error "Falha ao parar o serviço WildFly. Certifique-se de ter as permissões adequadas."
+# Parar o serviço WildFly
+msg "Parando o serviço WildFly..."
+if ! sudo service wildfly stop; then
+  warn "Falha ao parar o serviço WildFly. Tentando encerrar com kill -9..."
+  PID=$(pgrep -f 'wildfly')
+  if [ -n "$PID" ]; then
+    kill -9 $PID && msg "WildFly encerrado com sucesso via kill -9." || {
+      error "Não foi possível encerrar o processo WildFly."
+      exit 1
+    }
+  else
+    error "Processo WildFly não encontrado."
+    exit 1
+  fi
+fi
+
+# Verificar e baixar o script aplicarAtualizacao.sh
+if [ ! -f "aplicarAtualizacao.sh" ]; then
+  msg "Baixando o script de atualização..."
+  wget -q "http://a7.net.br/scherrer/aplicarAtualizacao.sh" || {
+    error "Falha ao baixar o script aplicarAtualizacao.sh."
+    exit 1
+  }
+else
+  msg "Script aplicarAtualizacao.sh já existe. Pulando o download."
+fi
+
+# Aplicar a atualização
+msg "Aplicando o pacote de atualização..."
+bash "aplicarAtualizacao.sh" "$PACOTE" || {
+  error "Falha ao aplicar a atualização. Verifique o script e as permissões."
   exit 1
 }
 
-# Applying the update script
-wget -q "http://a7.net.br/scherrer/aplicarAtualizacao.sh" && bash "aplicarAtualizacao.sh" "$PACOTE" || {
-  error "Falha ao aplicar a atualização. Verifique se o script de atualização está acessível e se a execução é permitida."
-  exit 1
-}
-
-# Starting the WildFly service
+# Iniciar o serviço WildFly
+msg "Iniciando o serviço WildFly..."
 sudo service wildfly start || {
-  error "Falha ao iniciar o serviço WildFly. Certifique-se de ter as permissões adequadas."
+  error "Falha ao iniciar o serviço WildFly. Verifique se há permissões adequadas."
   exit 1
 }
 
